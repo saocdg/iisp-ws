4f929f5eb6a1d3abb71f29880b37933c3ab56cf4
saocdg <ldegula.etel@gmail.com>
2015-05-15 12:06:48 +0800

    First commit

---
+++ webapproot/project.a.js
@@ -0,0 +1,33 @@
+wm.componentList['example.GoogleOrgChart'] = ['wm.base.widget.Composite','common.packages.example.GoogleOrgChart'];
+dojo.declare("SmplPOS", wm.Application, {
+	"disableDirtyEditorTracking": false, 
+	"eventDelay": 0, 
+	"hintDelay": 1500, 
+	"i18n": false, 
+	"isSecurityEnabled": false, 
+	"main": "Main", 
+	"manageHistory": true, 
+	"manageURL": false, 
+	"name": "", 
+	"phoneGapLoginPage": "Login", 
+	"phoneMain": "", 
+	"projectSubVersion": "Alpha", 
+	"projectVersion": 1, 
+	"sessionExpirationHandler": "navigateToLogin", 
+	"studioVersion": "6.7.0.RELEASE", 
+	"tabletMain": "", 
+	"theme": "wm.base.widget.themes.wm_default", 
+	"toastPosition": "br", 
+	"touchToClickDelay": 500, 
+	"touchToRightClickDelay": 1500,
+	"widgets": {
+		silkIconList: ["wm.ImageList", {"colCount":39,"height":16,"iconCount":90,"url":"lib/images/silkIcons/silk.png","width":16}, {}]
+	},
+	_end: 0
+});
+
+SmplPOS.extend({
+
+	_end: 0
+});
+SmplPOS.prototype._css = '';
